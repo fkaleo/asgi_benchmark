acf8587cda0f227fe9576c9cf2f424d92b3e0a40
Florian Boucault <florian.boucault@grafana.com>
2021-11-15 21:01:35 +0100

    first

---
+++ single.js
@@ -0,0 +1,26 @@
+import http from 'k6/http';
+
+
+export let options = {
+    vus: 1,
+    duration: '10s',
+    ext: {
+      loadimpact: {
+        projectID: 3542013,
+        name: "asgi_benchmark"
+      }
+    }
+}
+
+export function setup() {
+}
+
+const http_server_url = 'http://localhost:8000';
+
+export default function (data) {
+  const url = `${http_server_url}/`;
+  const res =  http.get(url)
+}
+
+export function teardown(data) {
+}
